805b6847eefa4951b6105bf4029e191eed87f443
Goff Patt <goffxnca@gmail.com>
2023-07-05 18:05:00 +0700

    use default ImageGallery image&thumbnail settings

--- components/Posts/PostImageGallery.js
+++ components/Posts/PostImageGallery.js
@@ -11,35 +11,35 @@ const PostImageGallery = ({ images }) => {
       showPlayButton={false}
       showIndex={true}
       showBullets={true}
-      renderItem={(item) => (
-        <div className="max-h-[80vh] h-[600px] w-full">
-          <div className="">
-            {/* <Image
-              className="image-gallery-image"
-              src={item.original}
-              layout="fill"
-              objectFit="contain"
-            ></Image> */}
+      // renderItem={(item) => (
+      //   <div className="max-h-[80vh] h-[600px] w-full">
+      //     <div className="">
+      //       {/* <Image
+      //         className="image-gallery-image"
+      //         src={item.original}
+      //         layout="fill"
+      //         objectFit="contain"
+      //       ></Image> */}
 
-            <div className="flex justify-center">
-              <img className="" src={item.original}></img>
-            </div>
-          </div>
-        </div>
-      )}
-      renderThumbInner={(item) => (
-        // <Image
-        //   src={item.thumbnail}
-        //   width={184}
-        //   height={200}
-        //   objectFit="cover"
-        // ></Image>
+      //       <div className="flex justify-center">
+      //         <img className="" src={item.original}></img>
+      //       </div>
+      //     </div>
+      //   </div>
+      // )}
+      // renderThumbInner={(item) => (
+      //   // <Image
+      //   //   src={item.thumbnail}
+      //   //   width={184}
+      //   //   height={200}
+      //   //   objectFit="cover"
+      //   // ></Image>
 
-        <img
-          src={item.thumbnail}
-          className="h-[100px] w-[92px] object-cover"
-        ></img>
-      )}
+      //   <img
+      //     src={item.thumbnail}
+      //     className="h-[100px] w-[92px] object-cover"
+      //   ></img>
+      // )}
     />
   );
 };
