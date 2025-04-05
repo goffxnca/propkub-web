a80a0361e5e3b248047acfd1ea5c9569ded9d371
Goff Phatt <goffxnca@gmail.com>
2025-04-05 11:05:00 +0700

    Turn on email sending again

--- functions/libs/mail.js
+++ functions/libs/mail.js
@@ -10,26 +10,25 @@ const sendEmail = async ({
   to,
 }) => {
   functions.logger.info("sendEmail Called with immediate return");
-  // sgMail.setApiKey(
-  //   "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"
-  // ); //put in env later
+  sgMail.setApiKey(
+    "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"
+  ); //put in env later
 
-  // const msg = {
-  //   to, //dynamic later
-  //   from,
-  //   // subject: "ยินดีต้อนรับสมาชิกใหม่อีกครั้ง",
-  //   // text: "คุณเป็นสมาชิกใหม่บนระบบของเรา ยินดีด้วย",
-  //   // html: "<h2>คุณสามารถเข้าสู่ระบบได้ทันทีเมื่อไหร่ก็ได้ที่คุณต้องการ</h2>",
-  //   template_id: templateId,
-  //   dynamic_template_data: {
-  //     ...templateData,
-  //     titlePrefix: IS_PROD ? "" : "[TEST]",
-  //   },
-  //   bcc: "phattharawit.s@gmail.com", //add to support@propkub.com later & once stable dropped.
-  // };
+  const msg = {
+    to, //dynamic later
+    from,
+    // subject: "ยินดีต้อนรับสมาชิกใหม่อีกครั้ง",
+    // text: "คุณเป็นสมาชิกใหม่บนระบบของเรา ยินดีด้วย",
+    // html: "<h2>คุณสามารถเข้าสู่ระบบได้ทันทีเมื่อไหร่ก็ได้ที่คุณต้องการ</h2>",
+    template_id: templateId,
+    dynamic_template_data: {
+      ...templateData,
+      titlePrefix: IS_PROD ? "" : "[TEST]",
+    },
+    bcc: "phattharawit.s@gmail.com", //add to support@propkub.com later & once stable dropped.
+  };
 
-  // return sgMail.send(msg);
-  return;
+  return sgMail.send(msg);
 };
 
 module.exports = {
