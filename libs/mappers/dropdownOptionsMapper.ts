c2f2098d230b9b7019629a402f53fdf5f4f7ab23
Goff Patt <goffxnca@gmail.com>
2025-04-12 10:34:37 +0700

    feat: convert dropdownOptionsMapper to ts

---
+++ libs/mappers/dropdownOptionsMapper.ts
@@ -0,0 +1,12 @@
+import { DropdownOption } from '../../src/types/misc/dropdownOption';
+
+const getDropdownOptions = (total: number): DropdownOption[] => {
+  const baseOption: DropdownOption = { id: 0, label: "0" };
+  const options: DropdownOption[] = Array.from({ length: total }, (_, i) => ({
+    id: i + 1,
+    label: String(i + 1)
+  }));
+  return [baseOption, ...options];
+};
+
+export { getDropdownOptions };
