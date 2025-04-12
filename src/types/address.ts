1decfd50c964453d6e73a55c584a0e558e834ea9
Goff Patt <goffxnca@gmail.com>
2025-04-12 09:29:52 +0700

    Add TypeScript types to addressManager.ts

--- src/types/address.ts
+++ src/types/address.ts
@@ -3,4 +3,30 @@ export interface Address {
   provinceLabel: string;
   districtLabel: string;
   subDistrictLabel: string;
+}
+
+export interface Province {
+  id: string;
+  name: string;
+  regionId: string;
+}
+
+export interface District {
+  id: string;
+  name: string;
+  provinceId: string;
+}
+
+export interface SubDistrict {
+  id: string;
+  name: string;
+  districtId: string;
+}
+
+export interface LocationBreadcrumb {
+  id: string;
+  name: string;
+  type: 'pv' | 'dt' | 'sd';
+  provinceId?: string;
+  districtId?: string;
 } 
