8d3a855cc10a79e22c1faf89af6ad514cec916fb
Goff Phatt <goffxnca@gmail.com>
2025-04-12 10:39:53 +0700

    feat: add TypeScript types to firebaseErrorCodeMapper

--- libs/mappers/firebaseErrorCodeMapper.ts
+++ libs/mappers/firebaseErrorCodeMapper.ts
@@ -1,8 +1,14 @@
-const firebaseErrors = [
+interface FirebaseError {
+  id: string;
+  label: string;
+}
+
+const firebaseErrors: FirebaseError[] = [
   { id: "auth/email-already-in-use", label: "อีเมลนี้ไม่สามารถใช้งานได้" },
   { id: "auth/sth-else", label: "ข้อผิดพลาดบลาๆ" },
 ];
-const getFirebaseErrorLabel = (errorId) => {
+
+const getFirebaseErrorLabel = (errorId: string): string => {
   const error = firebaseErrors.find((a) => a.id === errorId);
   if (!error) {
     console.log("firebaseErrorCodeMapper Failed:", errorId);
