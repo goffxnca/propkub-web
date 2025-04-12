73bf01853aee47b8945dc95d789e6d216781ff05
Goff Patt <goffxnca@gmail.com>
2025-04-12 10:30:35 +0700

    Add TypeScript types to assetTypeMapper.ts and move AssetType to misc

--- libs/mappers/assetTypeMapper.ts
+++ libs/mappers/assetTypeMapper.ts
@@ -1,11 +1,14 @@
-const assetTypes = [
+import { AssetType } from '../../src/types/misc/assetType';
+
+const assetTypes: AssetType[] = [
   { id: "condo", label: "คอนโด" },
   { id: "townhome", label: "ทาวน์โฮม" },
   { id: "house", label: "บ้านเดี่ยว" },
   { id: "land", label: "ที่ดิน" },
   // { id: "commercial", label: "อาคารพาณิชย์" },
 ];
-const getAssetType = (assetType) => {
+
+const getAssetType = (assetType: string): string => {
   return assetTypes.find((a) => a.id === assetType)?.label ?? "N/A";
 };
 
