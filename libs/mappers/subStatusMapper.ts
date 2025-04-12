74df9d2ee22360d21790ba5876746c0fd63bac27
Goff Patt <goffxnca@gmail.com>
2025-04-12 11:00:43 +0700

    refactor: add TypeScript types to subStatusMapper

--- libs/mappers/subStatusMapper.ts
+++ libs/mappers/subStatusMapper.ts
@@ -1,14 +1,14 @@
-const postSubStatuses = [
+import { PostSubStatus } from '../../src/types/misc/postSubStatus';
+
+const postSubStatuses: PostSubStatus[] = [
   { id: "created", label: "สร้างประกาศ" },
   { id: "fulfilled", label: "ปิดการขาย" },
   { id: "expired", label: "หมดอายุ" },
   { id: "closed", label: "ปิดประกาศ" },
 ];
-const getSubStatusLabelById = (subStatusId) => {
-  return (
-    postSubStatuses.find((subStatus) => subStatus.id === subStatusId)?.label ??
-    "N/A"
-  );
+
+const getSubStatusLabelById = (subStatusId: string): string => {
+  return postSubStatuses.find((subStatus) => subStatus.id === subStatusId)?.label ?? "N/A";
 };
 
 export { postSubStatuses, getSubStatusLabelById };
