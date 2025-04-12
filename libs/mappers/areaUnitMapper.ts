27c2c2a9c9438922f9c0f08a35dc38ceb1384c54
Goff Patt <goffxnca@gmail.com>
2025-04-12 09:45:31 +0700

    Add TypeScript types to areaUnitMapper.ts

--- libs/mappers/areaUnitMapper.ts
+++ libs/mappers/areaUnitMapper.ts
@@ -1,19 +1,22 @@
-const areaUnits = [
+import { AreaUnit } from "../../src/types/areaUnit";
+
+const areaUnits: AreaUnit[] = [
   { id: "whole", label: "ยกแปลง", ignorePrefix: true },
   { id: "sqm", label: "ตรม." },
   { id: "sqw", label: "ตรว." },
   { id: "ngan", label: "งาน" },
   { id: "rai", label: "ไร่" },
 ];
-const getAreaUnitById = (areaUnit) => {
+
+const getAreaUnitById = (areaUnit: string): string => {
   return areaUnits.find((a) => a.id === areaUnit)?.label ?? "N/A";
 };
 
-const getStandardAreaUnits = () => {
+const getStandardAreaUnits = (): AreaUnit[] => {
   return areaUnits.filter((a) => a.id !== "whole");
 };
 
-const getAllAreaUnits = () => {
+const getAllAreaUnits = (): AreaUnit[] => {
   return areaUnits;
 };
 
