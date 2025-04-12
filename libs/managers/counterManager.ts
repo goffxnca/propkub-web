670563694de014209a7f543cfc1b508e96ddb8eb
Goff Phatt <goffxnca@gmail.com>
2025-04-12 09:33:12 +0700

    Add TypeScript types to counterManager.ts

--- libs/managers/counterManager.ts
+++ libs/managers/counterManager.ts
@@ -1,16 +1,17 @@
 import { collection, doc, getDoc } from "firebase/firestore";
 import { db } from "../firebase";
+import { Counter } from "../../src/types/counter";
 
 const counterCollectionRef = collection(db, "counters");
 
-const getCounterById = async (counterId) => {
+const getCounterById = async (counterId: string): Promise<Counter> => {
   const docRef = doc(counterCollectionRef, counterId);
   const docSnap = await getDoc(docRef);
   const data = docSnap.data();
   return {
     id: docSnap.id,
     ...data,
-  };
+  } as Counter;
 };
 
 export { getCounterById };
