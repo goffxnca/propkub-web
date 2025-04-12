1571009e692bdf96954f5b07aca64c7eb7e58ad5
Goff Phatt <goffxnca@gmail.com>
2025-04-12 09:37:38 +0700

    Add TypeScript types to notificationManager.ts

---
+++ libs/managers/notificationManager.ts
@@ -0,0 +1,32 @@
+import { httpsCallable, HttpsCallableResult } from "firebase/functions";
+import { firebaseFunctions } from "../firebase";
+import {
+  GetNotificationsParams,
+  GetNotificationsResponse,
+  ReadNotificationParams,
+  ReadNotificationResponse,
+} from "../../src/types/Notification";
+
+const getNotifications = async ({
+  userId,
+}: GetNotificationsParams): Promise<GetNotificationsResponse> => {
+  const getNotificationsRef = httpsCallable(
+    firebaseFunctions,
+    "getNotifications"
+  );
+  const result = await getNotificationsRef({ userId });
+  return (result as HttpsCallableResult<GetNotificationsResponse>).data;
+};
+
+const readNotification = async ({
+  notificationId,
+}: ReadNotificationParams): Promise<ReadNotificationResponse> => {
+  const readNotificationRef = httpsCallable(
+    firebaseFunctions,
+    "readNotification"
+  );
+  const result = await readNotificationRef({ notificationId });
+  return (result as HttpsCallableResult<ReadNotificationResponse>).data;
+};
+
+export { getNotifications, readNotification };
