ed329f0530e76c325b53d0db3212280bf12d959e
Goff Phatt <goffxnca2@gmail.com>
2025-07-22 14:53:33 +0700

    Generate land province sitemap dynamically

---
+++ pages/land-province-sitemap.xml.js
@@ -0,0 +1,36 @@
+import { fetchProvinces } from "../libs/managers/addressManager";
+
+function generateSiteMap(posts) {
+  return `<?xml version="1.0" encoding="UTF-8"?>
+   <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+     ${posts
+       .map(({ id, name }) => {
+         return `
+       <url>
+            <loc>https://propkub.com/land/spv${id}/ขายที่ดิน-${name}</loc>
+            <priority>0.9</priority>
+       </url>
+     `;
+       })
+       .join("")}
+   </urlset>
+ `;
+}
+
+const LandProvinceSitemap = () => {};
+
+export async function getServerSideProps({ res }) {
+  const provinces = await fetchProvinces();
+  const sitemap = generateSiteMap(provinces);
+
+  // Send the XML to the browser
+  res.setHeader("Content-Type", "text/xml");
+  res.write(sitemap);
+  res.end();
+
+  return {
+    props: {},
+  };
+}
+
+export default LandProvinceSitemap;
