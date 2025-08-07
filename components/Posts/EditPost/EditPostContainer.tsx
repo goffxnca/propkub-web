8a66f6c2257cad4d4ac55e9cfef68621f80bb386
Goff Patt <goffxnca@gmail.com>
2025-08-07 19:42:51 +0700

    Update post related routings

---
+++ components/Posts/EditPost/EditPostContainer.tsx
@@ -0,0 +1,67 @@
+import { useState, useEffect } from "react";
+import { useRouter } from "next/router";
+import Head from "next/head";
+import Loader from "../../UI/Common/modals/Loader";
+import Modal from "../../UI/Public/Modal";
+import { apiClient } from "../../../libs/client";
+import { genPageTitle } from "../../../libs/seo-utils";
+import { ExclamationCircleIcon } from "@heroicons/react/outline";
+import AddPostForm from "../../../components/Posts/AddPost/AddPostForm";
+
+const EditPostContainer = ({ postId }) => {
+  const router = useRouter();
+  const [post, setPost] = useState(null);
+  const [fetching, setFetching] = useState(true);
+  const [error, setError] = useState("");
+
+  useEffect(() => {
+    const fetchPostData = async () => {
+      setFetching(true);
+      setError("");
+
+      try {
+        const postData = await apiClient.posts.getByIdForOwner(postId);
+        setPost(postData);
+      } catch (err) {
+        console.error("Error fetching a post:", err);
+        setError("เกิดข้อผิดพลาดในการโหลดข้อมูลประกาศ กรุณาลองใหม่อีกครั้ง");
+        setPost(null);
+      } finally {
+        setFetching(false);
+      }
+    };
+
+    fetchPostData();
+  }, [postId]);
+
+  if (fetching) return <Loader />;
+
+  if (error) {
+    return (
+      <Modal
+        visible={true}
+        title="เกิดข้อผิดพลาด"
+        type="warning"
+        desc={error}
+        buttonCaption="กลับไปแดชบอร์ด"
+        Icon={ExclamationCircleIcon}
+        onClose={() => {
+          router.push("/dashboard");
+        }}
+      />
+    );
+  }
+
+  if (!post) return null;
+
+  return (
+    <>
+      <Head>
+        <title>{genPageTitle(`แก้ไขประกาศหมายเลข ${post.postNumber}`)}</title>
+      </Head>
+      <AddPostForm postData={post} />
+    </>
+  );
+};
+
+export default EditPostContainer;
