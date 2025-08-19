165ce5eedbd0ba4a344358c7d157d3bdabc88b76
Goff Patt <goffxnca@gmail.com>
2025-08-19 21:39:44 +0700

    Remove using React.FC component style

--- components/Posts/PostStatusBadge/PostStatusBadge.tsx
+++ components/Posts/PostStatusBadge/PostStatusBadge.tsx
@@ -1,20 +1,19 @@
 import React from "react";
 import Badge from "../../UI/Badge/Badge";
-import { getStatusLabelById, getStatusVariantById } from "../../../libs/mappers/statusMapper";
+import {
+  getStatusLabelById,
+  getStatusVariantById,
+} from "../../../libs/mappers/statusMapper";
 
 interface PostStatusBadgeProps {
   status: string;
 }
 
-const PostStatusBadge: React.FC<PostStatusBadgeProps> = ({ status }) => {
+const PostStatusBadge = ({ status }: PostStatusBadgeProps) => {
   const variant = getStatusVariantById(status);
   const label = getStatusLabelById(status);
 
-  return (
-    <Badge variant={variant}>
-      {label}
-    </Badge>
-  );
+  return <Badge variant={variant}>{label}</Badge>;
 };
 
-export default PostStatusBadge; 
+export default PostStatusBadge;
