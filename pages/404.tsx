18f7029855a3c01635e3551c107d121f7a615f0b
Goff Patt <goffxnca@gmail.com>
2025-08-21 19:47:37 +0700

    Add prettier explicit rules

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,4 +1,12 @@
-import Link from "next/link";
+import Link from 'next/link';
+
+const nums = [
+  1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27,
+  28, 29, 30
+];
+
+const message =
+  'This is a very very very very very very long string that will break earlier at 80 chars.';
 
 const PageNotFound = () => {
   return (
@@ -14,9 +22,7 @@ const PageNotFound = () => {
       <div className="bg-white min-h-full px-4 py-16 sm:px-6 sm:py-24 md:grid md:place-items-center lg:px-8">
         <div className="max-w-max mx-auto">
           <main className="sm:flex">
-            <p className="text-4xl font-extrabold text-indigo-600 sm:text-5xl">
-              404
-            </p>
+            <p className="text-4xl font-extrabold text-indigo-600 sm:text-5xl">404</p>
             <div className="sm:ml-6">
               <div className="sm:border-l sm:border-gray-200 sm:pl-6">
                 <h1 className="text-4xl font-extrabold text-gray-900 tracking-tight sm:text-5xl">
