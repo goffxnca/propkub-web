e87a60ad95d7b549beb5215dd106baea69b2befc
Goff Patt <goffxnca@gmail.com>
2025-10-28 15:26:55 +0700

    Add enums user role and auth provider

--- types/models/user.ts
+++ types/models/user.ts
@@ -1,10 +1,22 @@
+export enum AuthProvider {
+  EMAIL = 'email',
+  GOOGLE = 'google',
+  FACEBOOK = 'facebook'
+}
+
+export enum UserRole {
+  NORMAL = 'normal',
+  AGENT = 'agent',
+  ADMIN = 'admin'
+}
+
 export interface User {
   _id: string;
   name: string;
   email: string;
   emailVerified: boolean;
-  provider: string;
-  role: string;
+  provider: AuthProvider;
+  role: UserRole;
   tos: boolean;
   cid: number;
   phone?: string;
