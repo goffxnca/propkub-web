b7079c8952c7451745086fd632f0fb2d594d4e6e
Goff Patt <goffxnca@gmail.com>
2025-11-01 23:28:51 +0700

    Add props type for Heading

--- components/UI/Public/Heading.tsx
+++ components/UI/Public/Heading.tsx
@@ -1,10 +1,17 @@
-const Heading = ({ size = 1, label }) => {
-  const HeadingTag = `h${size}`;
+import { ReactNode, createElement } from 'react';
 
-  return (
-    <HeadingTag className="text-lg font-bold text-gray-dark mb-2">
-      {label}
-    </HeadingTag>
+interface HeadingProps {
+  size?: 1 | 2 | 3 | 4 | 5 | 6;
+  label: ReactNode;
+}
+
+const Heading = ({ size = 1, label }: HeadingProps) => {
+  const tagName = `h${size}` as 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6';
+
+  return createElement(
+    tagName,
+    { className: 'text-lg font-bold text-gray-dark mb-2' },
+    label
   );
 };
 
