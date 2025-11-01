38f8d00851666e1e4c23b0031cb19bad995aa00a
Goff Patt <goffxnca@gmail.com>
2025-11-01 23:32:39 +0700

    Add props type for Card

--- components/UI/Public/Card.tsx
+++ components/UI/Public/Card.tsx
@@ -1,4 +1,10 @@
-const Card = ({ children }) => {
+import { ReactNode } from 'react';
+
+interface CardProps {
+  children: ReactNode;
+}
+
+const Card = ({ children }: CardProps) => {
   return (
     <div className="rounded-lg border border-solid border-gray-200 p-4 my-1">
       {children}
