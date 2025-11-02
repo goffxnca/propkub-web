9a4c59c3f1cdd5e020eb8ebcb0aebe649ae885ed
Goff Patt <goffxnca@gmail.com>
2025-11-02 19:03:11 +0700

    Upgrade misc utils to TS

--- libs/misc.ts
+++ libs/misc.ts
@@ -1,4 +1,4 @@
-export const sleep = (seconds = 5) => {
+export const sleep = (seconds: number = 5): Promise<void> => {
   console.log(`💤 Sleeping for ${seconds} seconds...`);
   return new Promise((resolve) => setTimeout(resolve, seconds * 1000));
 };
