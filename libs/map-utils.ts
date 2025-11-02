20241e4945a1bb3e6b939c2cc7640eb78dfd4473
Goff Patt <goffxnca@gmail.com>
2025-11-02 19:09:32 +0700

    Upgrade map utils to TS

--- libs/map-utils.ts
+++ libs/map-utils.ts
@@ -1,9 +1,17 @@
-const googleMapGeoCodeToAddress = (geoCodeResults = []) => {
+interface GeocodeAddressComponent {
+  long_name?: string;
+  short_name?: string;
+  types?: string[];
+}
+
+const googleMapGeoCodeToAddress = (
+  geoCodeResults: GeocodeAddressComponent[] = []
+): string => {
   let fullAddress = '';
   geoCodeResults.forEach((addressInfo) => {
-    fullAddress = fullAddress + ' ' + addressInfo.long_name || '';
+    fullAddress = fullAddress + ' ' + (addressInfo.long_name || '');
   });
-  return fullAddress;
+  return fullAddress.trim();
 };
 
 export { googleMapGeoCodeToAddress };
