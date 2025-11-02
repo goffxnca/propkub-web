542241f601537f22fd76ea351a01d09372e3ed0c
Goff Patt <goffxnca@gmail.com>
2025-11-02 20:06:32 +0700

    Upgrade types for hello.ts

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,5 +1,14 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-export default async function handler(req, res) {
+import type { NextApiRequest, NextApiResponse } from 'next';
+
+type HelloResponse = {
+  name: string;
+};
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<HelloResponse>
+) {
   res.status(200).json({ name: 'John Doe' });
 }
