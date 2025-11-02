8fc0755e1996f2dd91ccc7494aadd1f5a41f0318
Goff Patt <goffxnca@gmail.com>
2025-11-02 19:51:04 +0700

    Upgrade object utils to TS

--- libs/object-utils.ts
+++ libs/object-utils.ts
@@ -1,7 +1,7 @@
-export const cleanObject = (obj) => {
+export const cleanObject = <T extends Record<string, any>>(obj: T): Partial<T> => {
   return Object.fromEntries(
     Object.entries(obj).filter(
       ([_, v]) => v !== null && v !== undefined && v !== ''
     )
-  );
+  ) as Partial<T>;
 };
