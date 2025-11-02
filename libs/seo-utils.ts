4aa5755615f0626335fd049869e71052ba15cc02
Goff Patt <goffxnca@gmail.com>
2025-11-02 19:38:20 +0700

    Upgrade seo utils to TS

--- libs/seo-utils.ts
+++ libs/seo-utils.ts
@@ -1,18 +1,18 @@
-const genPageTitle = (title) => {
+const genPageTitle = (title?: string) => {
   return title
-    ? `${title.substring()} | PropKub.com`
+    ? `${title} | PropKub.com`
     : 'PropKub.com - ลงประกาศขาย/ให้เช่า คอนโด บ้าน ที่ดินฟรี';
 };
 
-const genPropertyTitleMeta = (title) => {
+const genPropertyTitleMeta = (title?: string) => {
   return title ? `${title.substring(0, 65)}` : '';
 };
 
-const genPropertyDescriptionMeta = (htmlDesc) => {
+const genPropertyDescriptionMeta = (htmlDesc: string) => {
   return htmlDesc.replace(/<\/?[^>]+(>|$)/g, '').substring(0, 170);
 };
 
-function getCanonicalUrl(fullUrl) {
+function getCanonicalUrl(fullUrl: string) {
   return fullUrl.split('?')[0];
 }
 
